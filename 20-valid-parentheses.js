4512500316dff897672674f3892eb661cb5364f6
tetsuya_kojima <gangelion8@gmail.com>
2023-06-08 22:10:54 +0900

    cheat: pass but saw the answer

---
+++ 20-valid-parentheses.js
@@ -0,0 +1,12 @@
+function isValid(s) {
+  let stack = []
+  let store = { ')': '(', '}': '{', ']': '[' }
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length < 1) stack.push(s[i]) // add to stack if nothing in
+    else if (stack[stack.length - 1] !== store[s[i]]) stack.push(s[i]) // push to stack if top char in stack not closing current char
+    else stack.pop() // Here remove the top element in stack, this will be closing that element i.e. we'll have e.g. '(' in stack and current value is ')'
+  }
+
+  return stack.length === 0
+}
