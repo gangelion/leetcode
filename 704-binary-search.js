fcf2bd3d6f0a4fc3b7260185c504c153928d4f99
tetsuya_kojima <gangelion8@gmail.com>
2023-06-09 20:02:57 +0900

    pass: learn binary search

---
+++ 704-binary-search.js
@@ -0,0 +1,12 @@
+function search(nums, target) {
+  let l = 0
+  let r = nums.length - 1
+
+  while (l <= r) {
+    const m = Math.ceil((l + r) / 2)
+    if (nums[m] > target) r = m - 1
+    else if (nums[m] < target) l = m + 1
+    else return m
+  }
+  return -1
+}
