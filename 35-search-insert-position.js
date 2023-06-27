cff1940144dab5e588d622b579cf77d99ce0e6e0
tetsuya_kojima <gangelion8@gmail.com>
2023-06-27 21:32:10 +0900

    cheat: binary way

--- 35-search-insert-position.js
+++ 35-search-insert-position.js
@@ -1,8 +1,8 @@
-function searchInsert(nums, target) {
-  nums.push(target)
-  nums.sort((a, b) => a - b)
-  return nums.indexOf(target)
-}
+// function searchInsert(nums, target) {
+//   nums.push(target)
+//   nums.sort((a, b) => a - b)
+//   return nums.indexOf(target)
+// }
 
 // function searchInsert(nums, target) {
 //   if (nums[0] >= target) return 0
@@ -12,3 +12,17 @@ function searchInsert(nums, target) {
 //     if (target > nums[i] && target <= nums[i + 1]) return i + 1
 //   }
 // }
+
+function searchInsert(nums, target) {
+  let l = 0
+  let r = nums.length - 1
+
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2)
+    if (target === nums[m]) return m
+    else if (target > nums[m]) l = m + 1
+    else r = m - 1
+  }
+
+  return l
+}
