d6c5161cc0c2c2ee7163d83a28c4ff74c2046b1c
tetsuya_kojima <gangelion8@gmail.com>
2023-06-29 20:10:05 +0900

    cheat: optimize code

--- 58-length-of-last-word.js
+++ 58-length-of-last-word.js
@@ -1,12 +1,4 @@
 function lengthOfLastWord(s) {
-  let count = 0
-  let flag = false
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] !== ' ') {
-      flag = true
-      count++
-    }
-    else if (flag) return count
-  }
-  return count
-};
+  const words = s.split(' ').filter((v) => v)
+  return words[words.length - 1].length
+}
